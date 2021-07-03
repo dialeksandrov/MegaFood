829d6cb8cf270adffa47d2135b80e2a712d50000
dialeksandrov <dialeksandrov12@gmail.com>
2021-07-03 11:37:22 +0600

    First commit

---
+++ src/main/java/kg/aleksandrov/megafood/MegaFoodApplication.java
@@ -0,0 +1,13 @@
+package kg.aleksandrov.megafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MegaFoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/aleksandrov/megafood/MegaFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.aleksandrov.megafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
